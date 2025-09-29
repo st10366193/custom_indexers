a20c297c8a45e0230da919c223a3eda21a3ddb58
Katlego Makhuba <128791831+st10366193@users.noreply.github.com>
2025-09-29 11:13:30 +0000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace custom_indexers
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // creating an instace for store_indexes
+           
+
+            store_index get_index = new store_index();
+
+            // setting the value 
+            get_index[0] = "brocolli";
+
+            // getting the value
+            display_value.Text =get_index[0];
+
+        }
+    }
+}
---
+++ store_index.cs
@@ -0,0 +1,21 @@
+﻿namespace custom_indexers
+{
+    public class store_index
+    {
+        // global declaration
+
+        private string[] vegetables = new string[2];
+
+        // creating custom indexer method, to get and set the value
+
+        public string this[int index] 
+        {
+            // get the value at the index 
+            get { return vegetables[index]; }
+
+            // set the value at the index 
+            set { vegetables[index] = value; }
+        }
+            
+    }
+}
